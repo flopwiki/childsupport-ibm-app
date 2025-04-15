9fec232aeb79cad47215f00a27612c5c47445b03
flopwiki <flopwiki@bugcrowdninja.com>
2025-04-15 19:06:47 -0400

    close off payment and paymentrepository until April 20

--- childsupport/src/main/java/org/ibm/payment/processor/childsupport/application/PaymentController.java
+++ childsupport/src/main/java/org/ibm/payment/processor/childsupport/application/PaymentController.java
@@ -3,25 +3,26 @@ package org.ibm.payment.processor.childsupport.application;
 import org.ibm.payment.processor.childsupport.domain.Payment;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/payments")
-public class PaymentController {
-
-    private final PaymentService paymentService;
-
-    public PaymentController(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
-
-    @PostMapping("/process")
-    public String createPayment(@RequestBody Payment payment) {
-        paymentService.processPayment(payment);
-        return "Payment process successfully";
-    }
-
-    @GetMapping("/{id}")
-    public Payment getPayment(@PathVariable Long id) {
-        return paymentService.getPaymentById(id);
-    }
-
-}
+//@RestController
+//@RequestMapping("/api/payments")
+//public class PaymentController {
+//
+//    private final PaymentService paymentService;
+//
+//    public PaymentController(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
+//
+//    @PostMapping("/process")
+//    public String createPayment(@RequestBody Payment payment) {
+//        paymentService.processPayment(payment);
+//        return "Payment process successfully";
+//    }
+//
+//    @GetMapping("/{id}")
+//    public Payment getPayment(@PathVariable Long id) {
+//        //return paymentService.getPaymentById(id);
+//        return null;
+//    }
+//
+//}
--- childsupport/src/main/java/org/ibm/payment/processor/childsupport/repository/PaymentRepository.java
+++ childsupport/src/main/java/org/ibm/payment/processor/childsupport/repository/PaymentRepository.java
@@ -1,13 +1,15 @@
 package org.ibm.payment.processor.childsupport.repository;
 
 import org.ibm.payment.processor.childsupport.domain.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface PaymentRepository {
 
     void save(Payment payment);
-
 
 }
