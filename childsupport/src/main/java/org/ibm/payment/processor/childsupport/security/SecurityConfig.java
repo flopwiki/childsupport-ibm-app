ccea8ac6444562aeeaed0a753058de8dec43c359
flopwiki <flopwiki@bugcrowdninja.com>
2025-05-26 12:05:39 -0400

    using In-Memory Auth Override Properties based on application.properties

--- childsupport/src/main/java/org/ibm/payment/processor/childsupport/security/SecurityConfig.java
+++ childsupport/src/main/java/org/ibm/payment/processor/childsupport/security/SecurityConfig.java
@@ -15,32 +15,32 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/payments", "/help", "/login", "/error", "/css/**", "/js/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
-                )
-             .formLogin(withDefaults()
-//                .formLogin(form -> form
-//                        .loginPage("/login.html")
-//                        .defaultSuccessUrl("/")
-//                        .failureUrl("/login?error=true")
-//                        .permitAll()
-                )
-                .logout(withDefaults());
-//                .logout(logout -> logout
-//                        .logoutSuccessUrl("/login.html?logout=true")
-//                        .permitAll()
-//                );
-
-        return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/", "/payments", "/help", "/login", "/error", "/css/**", "/js/**").permitAll()
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+//                        .anyRequest().authenticated()
+//                )
+//             .formLogin(withDefaults()
+////                .formLogin(form -> form
+////                        .loginPage("/login.html")
+////                        .defaultSuccessUrl("/")
+////                        .failureUrl("/login?error=true")
+////                        .permitAll()
+//                )
+//                .logout(withDefaults());
+////                .logout(logout -> logout
+////                        .logoutSuccessUrl("/login.html?logout=true")
+////                        .permitAll()
+////                );
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 }
